f3cda683227517af9d8e3c1ae12e26cb43bd67f1
doniurek <aldonaglebicka@gmail.com>
2018-12-05 01:42:50 +0100

    Add chances counter

--- hangman.py
+++ hangman.py
@@ -1,7 +1,9 @@
 import random
 
-with open('sowpods.txt', 'r') as f:
-    words_base = f.read().split('\n')
+with open('sowpods.txt', 'r') as file:
+    words_base = file.read().split('\n')
+
+counter = 6
 
 def pick_a_word():
     return random.choice(words_base)
@@ -10,19 +12,30 @@ def guessing():
     printek = '\n'
     for i in field:
         printek += i + ' '
+    print("\nYou have", counter, "chances left.")
     print(printek)
     letter = input("\nGuess the letter: ")
     if letter.lower() in word or letter.upper() in word:
         for i in [pos for pos, char in enumerate(word) if char == letter.upper()]:
             field[i] = letter.upper()
+        return 0
     else:
-        print("\nWrong! Try again.")
+        if counter > 1:
+            print("\nWrong! Try again.")
+        else:
+            print("\nWrong!")
+
+        return 1
 
 x = pick_a_word()
 word = list(x)
 field = list(len(word)*'_')
+
 print("Welcome in the Hangman game! Discover the word, by guessing letter by letter.")
-print(word)
-while word != field:
-    guessing()
-print("Congrats, You won! The missed word was:", x)
+print(x)
+while word != field and counter > 0:
+    counter -= guessing()
+if word == field:
+    print("Congrats, You won! The missed word was:", x)
+else:
+    print("\nSorry, You lose. The missed word was:", x)
